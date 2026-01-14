672072b7873b1b8a1fd9edbfce9a5f76203b80ae
9869756272 <manubarca921@gmail.com>
2026-01-14 23:15:57 +0545

    Toast and Backdrop

---
+++ frontend/components/Backdrop.jsx
@@ -0,0 +1,37 @@
+import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+import state from '../store/index.js';
+
+function Backdrop() {
+  const shadows = useRef();
+  const snap = useSnapshot(state);
+  
+  useFrame((state, delta) => {
+    if (shadows.current && shadows.current.getMesh) {
+      easing.dampC(shadows.current.getMesh().material.color, snap.color, 0.25, delta);
+    }
+  });
+  
+  return (
+    <AccumulativeShadows
+      ref={shadows}
+      temporal
+      frames={60}
+      alphaTest={0.85}
+      scale={5}
+      resolution={2048}
+      rotation={[Math.PI / 2, 0, 0]}
+      position={[0, 0, -0.14]}
+    >
+      <RandomizedLight amount={4} radius={9} intensity={0.55 * Math.PI} ambient={0.25} position={[5, 5, -10]} />
+      <RandomizedLight amount={4} radius={5} intensity={0.25 * Math.PI} ambient={0.55} position={[-5, 5, -9]} />
+    </AccumulativeShadows>
+  );
+}
+
+export default Backdrop;
+
+
